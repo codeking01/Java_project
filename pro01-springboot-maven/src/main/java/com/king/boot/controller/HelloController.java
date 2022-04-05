fb46d47988e1b93962e06548978d5a4a30726318
codeking <1094234526@qq.com>
2022-04-05 14:32:10 +0800

    first commit

---
+++ pro01-springboot-maven/src/main/java/com/king/boot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.king.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// @ResponseBody
+// @Controller
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "Hello SpringBoot";
+    }
+}
